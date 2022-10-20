1e526a693a5950ab17f87f6af5093540f22836b1
Aleksey <d_team-lex@bk.ru>
2022-10-20 18:30:34 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,107 @@
 <template>
   <p>Start</p>
+
+  <Card
+    v-for="notification in notifications"
+    :key="notification.id"
+    :cardData="notification"
+  />
 </template>
 
 <script>
+import Card from '@/components/Card.vue';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      notifications: [
+        {
+          id: 1,
+          user: 'Mark Webber',
+          userAvatar: 'avatar-mark-webber.webp',
+          actionType: 'reacted',
+          actionText: 'reacted to your recent post',
+          actionData: 'My first tournament today!',
+          time: '1m ago',
+        },
+        {
+          id: 2,
+          user: 'Angela Gray',
+          userAvatar: 'avatar-angela-gray.webp',
+          actionType: 'followed',
+          actionText: 'followed you',
+          actionData: '',
+          time: '5m ago',
+        },
+        {
+          id: 3,
+          user: 'Jacob Thompson',
+          userAvatar: 'avatar-jacob-thompson.webp',
+          actionType: 'joined',
+          actionText: 'has joined your group',
+          actionData: 'Chess Club',
+          time: '1 day ago',
+        },
+        {
+          id: 4,
+          user: 'Rizky Hasanuddin',
+          userAvatar: 'avatar-rizky-hasanuddin.webp',
+          actionType: 'message',
+          actionText: 'sent you a private message',
+          actionData:
+            "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+          time: '5 days ago',
+        },
+        {
+          id: 5,
+          user: 'Kimberly Smith',
+          userAvatar: 'avatar-kimberly-smith.webp',
+          actionType: 'commented',
+          actionText: 'commented on your picture',
+          actionData: 'image-chess.webp',
+          time: '1 week ago',
+        },
+        {
+          id: 6,
+          user: 'Nathan Peterson',
+          userAvatar: 'avatar-nathan-peterson.webp',
+          actionType: 'reacted',
+          actionText: 'reacted to your recent post',
+          actionData: '5 end-game strategies to increase your win rate',
+          time: '2 weeks ago',
+        },
+        {
+          id: 7,
+          user: 'Anna Kim',
+          userAvatar: 'avatar-anna-kim.webp',
+          actionType: 'lefted',
+          actionText: 'left the group',
+          actionData: 'Chess Club',
+          time: '2 weeks ago',
+        },
+      ],
+    };
+  },
+  methods: {
+    getData() {
+      console.log('data');
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,105 @@
 <template>
-  <div class="card"></div>
+  <div class="card">
+    <img
+      class="card__avatar"
+      :src="`./images/${cardData.userAvatar}`"
+      alt=""
+      srcset=""
+    />
+    <div class="card__content">
+      <div class="card__info">
+        <a class="card__info-user-name" href="">{{ cardData.user }}</a>
+        <div class="card__info-action-text">{{ cardData.actionText }}</div>
+        <a class="card__info-action-data" v-if="setCardData" href="">
+          {{ cardData.actionData }}
+        </a>
+        <div class="card__led"></div>
+      </div>
+      <div class="card__time">{{ cardData.time }}</div>
+      <div
+        class="card__info-action-message"
+        v-if="cardData.actionType === 'message'"
+      >
+        {{ cardData.actionData }}
+      </div>
+    </div>
+    <img
+      class="card__info-action-image"
+      v-if="cardData.actionType === 'commented'"
+      :src="`./images/${cardData.userAvatar}`"
+      alt=""
+      srcset=""
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'c-card',
+  props: {
+    cardData: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+  computed: {
+    setCardData() {
+      return (
+        this.cardData.actionData &&
+        this.cardData.actionType !== 'message' &&
+        this.cardData.actionType !== 'commented'
+      );
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  align-items: center;
+  background: rgb(165, 165, 165);
+  padding: 10px;
+  margin-bottom: 10px;
+}
+/* .card__avatar {} */
+.card__content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 20px;
+}
+.card__info {
+  display: flex;
+  align-items: center;
+
+  &-user-name {
+    margin-right: 6px;
+    color: red;
+  }
+  &-action-text {
+    margin-right: 6px;
+    color: green;
+  }
+  &-action-data {
+    margin-right: 6px;
+    color: blue;
+  }
+}
+
+/* .card__user-name {} */
+.card__action-text {
+  padding: 0;
+  margin: 0 6px;
+}
+/* .card__action-data {} */
+.card__led {
+  display: flex;
+  width: 8px;
+  height: 8px;
+  background: red;
+  border-radius: 50%;
+}
+/* .card__time {} */
+</style>
